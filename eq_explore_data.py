5dcfafd4da3eb09ea99cc05c960300411e69b893
zahar <zaharambrozyak@gmail.com>
2021-07-07 23:03:11 +0300

    Some improvements

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,7 +1,10 @@
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 # Дослідити структуру даних.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_7_day_m1.json'
 with open(filename) as f:
      all_eq_data = json.load(f)
 
@@ -10,17 +13,31 @@ with open(readable_file, 'w') as f:
      json.dump(all_eq_data, f, indent=4)
 
 all_eq_dicts = all_eq_data['features']
+metadata = all_eq_data['metadata']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-     mag = eq_dict['properties']['mag']
-     lon = eq_dict['geometry']['coordinates'][0]
-     lat = eq_dict['geometry']['coordinates'][1]
+     mags.append(eq_dict['properties']['mag'])
+     lons.append(eq_dict['geometry']['coordinates'][0])
+     lats.append(eq_dict['geometry']['coordinates'][1])
+     hover_texts.append(eq_dict['properties']['title'])
+title = metadata['title']
 
-     mags.append(mag)
-     lons.append(lon)
-     lats.append(lat)
+# Нанести землетруси на мапу.
+data = [{
+     'type': 'scattergeo',
+     'lon': lons,
+     'lat': lats,
+     'text': hover_texts,
+     'marker': {
+          'size': [5*mag for mag in mags],
+          'color': mags,
+          'colorscale': 'Viridis',
+          'reversescale': True,
+          'colorbar': {'title': 'Magnitude'},
+     },
+}]
+my_layout = Layout(title=title)
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
